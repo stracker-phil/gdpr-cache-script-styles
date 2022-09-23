266d30951c5eca21d6590b22e4d335e24fcac692
Philipp Stracker <philipp@divimode.com>
2022-09-24 00:59:29 +0200

    Add staleness check

--- includes/libs/worker.php
+++ includes/libs/worker.php
@@ -14,6 +14,7 @@ defined( 'ABSPATH' ) || exit;
 // ----------------------------------------------------------------------------
 
 add_action( 'gdpr_cache_worker', __NAMESPACE__ . '\run_background_tasks' );
+add_action( 'gdpr_cache_check_staleness', __NAMESPACE__ . '\run_staleness_checks' );
 
 // ----------------------------------------------------------------------------
 
@@ -139,6 +140,28 @@ function is_worker_busy() {
 
 
 /**
+ * Prevents background worker from spawning.
+ *
+ * @since 1.0.4
+ * @return void
+ */
+function lock_worker() {
+	update_option( GDPR_CACHE_WORKER_LOCK, time() );
+}
+
+
+/**
+ * Allows spawning of a new background worker process.
+ *
+ * @since 1.0.4
+ * @return void
+ */
+function unlock_worker() {
+	delete_option( GDPR_CACHE_WORKER_LOCK );
+}
+
+
+/**
  * Runs the background worker tasks; is called via a cron event.
  *
  * @since 1.0.0
@@ -160,8 +183,7 @@ function run_background_tasks() {
 	 */
 	do_action( 'gdpr_cache_before_worker' );
 
-	// Lock the worker task.
-	update_option( GDPR_CACHE_WORKER_LOCK, time() );
+	lock_worker();
 
 	$queue = get_worker_queue();
 
@@ -180,12 +202,10 @@ function run_background_tasks() {
 
 		set_worker_queue( $queue );
 
-		// Update the process-lock
-		update_option( GDPR_CACHE_WORKER_LOCK, time() );
+		lock_worker();
 	}
 
-	// Unlock the worker task.
-	delete_option( GDPR_CACHE_WORKER_LOCK );
+	unlock_worker();
 
 	/**
 	 * Action that fires after the background worker finished all enqueued
@@ -196,3 +216,108 @@ function run_background_tasks() {
 	do_action( 'gdpr_cache_after_worker' );
 }
 
+
+/**
+ * When using the plugin for a longer period of time, it will cache many files.
+ * It can happen, that some of those cached assets are not used on the website
+ * anymore
+ *
+ * For example, when you switch to a different Google Font, the new font is
+ * cached, but the old font is not removed from cache.
+ *
+ * This function reviews the "last-used" timestamps to determine, which assets
+ * have become stale and should be removed from the website.
+ *
+ * A cron-job calls this function once per day.
+ *
+ * @since 1.0.4
+ * @return void
+ */
+function run_staleness_checks() {
+	if ( is_worker_busy() ) {
+		// Background worker is busy. Try again in a few seconds.
+		wp_schedule_single_event( time() + 15, 'gdpr_cache_check_staleness' );
+
+		return;
+	}
+
+	/**
+	 * Action that fires before the cron task starts to check for stale files.
+	 *
+	 * @since 1.0.4
+	 */
+	do_action( 'gdpr_cache_before_stale_check' );
+
+	lock_worker();
+
+	$fs    = get_filesystem();
+	$cache = get_cached_data();
+	$queue = get_worker_queue();
+
+	foreach ( $cache as $url => $data ) {
+		$stale_age = get_asset_staleness( $url );
+
+		/**
+		 * Determines if a file has become stale.
+		 *
+		 * By default, this is TRUE when the file was not served for 30 days.
+		 * Once the file is stale, it's removed from the cache.
+		 *
+		 * @since 1.0.4
+		 *
+		 * @param bool   $is_stale Whether the file is considered to be stale.
+		 * @param int    $age      How many hours ago the file was last served.
+		 * @param string $url      The remote source of the file.
+		 * @param array  $data     Details about the cache.
+		 */
+		$is_stale = apply_filters(
+			'gdpr_cache_is_file_stale',
+			$stale_age >= GDPR_CACHE_STALE_HOURS,
+			$stale_age,
+			$url,
+			$data
+		);
+
+		if ( ! $is_stale ) {
+			continue;
+		}
+
+		/**
+		 * Fires right before a stale file is removed from the DB
+		 *
+		 * @since 1.0.4
+		 *
+		 * @param string $url  The remote source of the file.
+		 * @param array  $data Details about the cache.
+		 */
+		do_action( 'gdpr_cache_remove_stale_file', $url, $data );
+
+		// Remove the file from the cache.
+		unset ( $cache[ $url ] );
+
+		// De-queue eventual background tasks.
+		unset ( $queue[ $url ] );
+
+		// Remove links to any dependencies.
+		set_dependencies( $url, null );
+
+		// Delete the local file.
+		$local_file = build_cache_file_path( $data['file'] );
+
+		if ( $fs->exists( $local_file ) ) {
+			$fs->delete( $local_file, false, 'f' );
+		}
+	}
+
+	set_cached_data( $cache );
+	set_worker_queue( $queue );
+
+	unlock_worker();
+
+	/**
+	 * Action that fires after the cron task finished checking for stale files.
+	 *
+	 * @since 1.0.4
+	 */
+	do_action( 'gdpr_cache_after_stale_check' );
+}
