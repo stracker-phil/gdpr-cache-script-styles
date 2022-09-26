3623811234ec8cbd7f0429190e314660dc22d401
Philipp Stracker <philipp@divimode.com>
2022-09-26 10:39:23 +0200

    Add a git-only folder for docs/assets

--- config.bash
+++ config.bash
@@ -3,11 +3,12 @@
 # that should be identical for every developer.
 # To adjust settings, edit the file "config.local.bash"
 #
-# plugin_name: The plugin slug
-# prefix:      The prefix of custom functions (used by copy-project.sh)
-# package:     The namespace/package name (used by copy-project.sh)
-# text_domain: The translation text-domain (used by copy-project.sh)
-# plugin_type: free or pro
+# plugin_name:   The plugin slug
+# prefix:        The prefix of custom functions (used by copy-project.sh)
+# package:       The namespace/package name (used by copy-project.sh)
+# text_domain:   The translation text-domain (used by copy-project.sh)
+# plugin_type:   free or pro
+# build_exclude: List of files to exclude from the build process (array).
 #
 
 export plugin_name=gdpr-cache-script-styles
@@ -15,6 +16,7 @@ export prefix=
 export package=GdprCache
 export text_domain=gdpr-cache
 export plugin_type=free
+export build_exclude=('/docs')
 
 # Local build targets (optional)
 # Local sites that should receive the plugin code for testing (after deploy.sh).
